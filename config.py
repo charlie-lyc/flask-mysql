c8a62653116000bca1c16432685595b80787b7db
charlie-lyc <ofetwo@gmail.com>
2021-08-03 00:22:52 +0900

    Include cofig.py when deploy

---
+++ config.py
@@ -0,0 +1,17 @@
+### 설치시 변경
+# MYSQL_HOST = 'mysql://b55f6c362f76c3:96860c7c@us-cdbr-east-04.cleardb.com/heroku_f92a588680a967f'
+#######################################
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'charlie-lyc'
+MYSQL_DB = 'flask_app'
+MYSQL_CURSORCLASS = 'DictCursor'
+#######################################
+
+
+
+#######################################
+WTF_CSRF_ENABLED = True
+WTF_CSRF_SECRET_KEY = 'flask-app'
+
+SECRET_KEY = 'flask-app'
