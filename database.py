8bfa7302cdeb5891a0a9df1c448e9a95c370f755
charlie-lyc <ofetwo@gmail.com>
2021-08-03 06:18:26 +0900

    Fix db config 15

--- database.py
+++ database.py
@@ -4,8 +4,8 @@ import mysql.connector
 config = {
     ### 설치시 변경
     # 'host': 'mysql://b55f6c362f76c3:96860c7c@us-cdbr-east-04.cleardb.com/heroku_f92a588680a967f',
-    'host': 'b55f6c362f76c3:96860c7c@us-cdbr-east-04.cleardb.com',
-    'user': 'ofetwo@gmail.com',
+    'host': 'us-cdbr-east-04.cleardb.com',
+    'user': 'b55f6c362f76c3:96860c7c',
     'password': 'charlie-lyc',
     ##########################
     # 'host': 'localhost',
