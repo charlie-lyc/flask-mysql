5d1d220bf0ac3a990213d17d5b8249dea8874eb8
charlie-lyc <ofetwo@gmail.com>
2021-08-03 07:19:04 +0900

    Fix db config 28

--- database.py
+++ database.py
@@ -3,16 +3,14 @@ import mysql.connector
 
 config = {
     ### 설치시 변경
-    # 'host': 'mysql://b55f6c362f76c3:96860c7c@us-cdbr-east-04.cleardb.com/heroku_f92a588680a967f',
+    ### mysql://b55f6c362f76c3:96860c7c@us-cdbr-east-04.cleardb.com/heroku_f92a588680a967f
+    'username': 'b55f6c362f76c3',
+    'password': '96860c7c',
     'host': 'us-cdbr-east-04.cleardb.com',
     'database': 'heroku_f92a588680a967f',
-    'user': 'b55f6c362f76c3',
-    ###########################
-    # ???
-    'password': '96860c7c',
     ##########################
     # 'host': 'localhost',
-    # 'user': 'root',
+    # 'username': 'root',
     # 'password': 'charlie-lyc',
     # 'database': 'flask_mysql', # 처음에 포함하지 않았다가, 데이터베이스가 생성된 이후에 포함 : create database if not exists {} default character set utf8;
 }
