b2a4479ef14a993c9c92f52dd42ea0a947bdebb0
charlie-lyc <ofetwo@gmail.com>
2021-08-03 05:58:34 +0900

    Fix db config 6

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ config = {
     ### 설치시 변경
     # 'host': 'mysql://b55f6c362f76c3:96860c7c@us-cdbr-east-04.cleardb.com/heroku_f92a588680a967f',
     'host': 'us-cdbr-east-04.cleardb.com',
-    'user': 'b55f6c362f76c3',
+    'user': 'ofetwo@gmail.com',
     'password': '96860c7c',
     ##########################
     # 'host': 'localhost',
