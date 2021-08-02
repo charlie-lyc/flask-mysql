bff613bb36ca23b8ee4a3b44fe45f661eae765f2
charlie-lyc <ofetwo@gmail.com>
2021-08-03 05:55:24 +0900

    Fix db config 5

--- database.py
+++ database.py
@@ -5,10 +5,12 @@ config = {
     ### 설치시 변경
     # 'host': 'mysql://b55f6c362f76c3:96860c7c@us-cdbr-east-04.cleardb.com/heroku_f92a588680a967f',
     'host': 'us-cdbr-east-04.cleardb.com',
-    # 'host': 'localhost',
+    'user': 'b55f6c362f76c3',
+    'password': '96860c7c',
     ##########################
-    'user': 'root',
-    'password': 'charlie-lyc',
+    # 'host': 'localhost',
+    # 'user': 'root',
+    # 'password': 'charlie-lyc',
     ##########################
     'database': 'flask_mysql' # 처음에 포함하지 않았다가, 데이터베이스가 생성된 이후에 포함 : create database if not exists {} default character set utf8;
 }
