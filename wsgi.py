e54b226744569b3499753e6eb604b4927ee53d93
charlie-lyc <ofetwo@gmail.com>
2021-08-03 00:19:03 +0900

    Try again

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     # app.debug = True
     # app.run()
     #####################
-    app.run(debug=True) 
+    # app.run(debug=True) 
 
     ###### 설치시 변경######
     app.run()
