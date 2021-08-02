5b85142ee68bf84bec6be2a8d41b1439fb9ff4d3
charlie-lyc <ofetwo@gmail.com>
2021-08-03 07:21:12 +0900

    Fix db config 29

--- database.py
+++ database.py
@@ -6,11 +6,11 @@ config = {
     ### mysql://b55f6c362f76c3:96860c7c@us-cdbr-east-04.cleardb.com/heroku_f92a588680a967f
     'username': 'b55f6c362f76c3',
     'password': '96860c7c',
-    'host': 'us-cdbr-east-04.cleardb.com',
-    'database': 'heroku_f92a588680a967f',
+    'hostname': 'us-cdbr-east-04.cleardb.com',
+    'database_name': 'heroku_f92a588680a967f',
     ##########################
     # 'host': 'localhost',
-    # 'username': 'root',
+    # 'user': 'root',
     # 'password': 'charlie-lyc',
     # 'database': 'flask_mysql', # 처음에 포함하지 않았다가, 데이터베이스가 생성된 이후에 포함 : create database if not exists {} default character set utf8;
 }
